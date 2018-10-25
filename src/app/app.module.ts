f136967583d7990f7d03c61bb37cea820389a960
lkl <964096968@qq.com>
2018-10-25 16:59:26 +0800

    “message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { routes } from './app-routing';
 import { MainModule } from './main/main.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 
 @NgModule({
@@ -14,9 +22,13 @@ import { MainModule } from './main/main.module';
   imports: [
     BrowserModule,
     MainModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 import { routes } from './main-routing';
 import { HomeComponent } from './home/home.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent,
   ],
   imports: [
+    NgZorroAntdModule,
     RouterModule.forChild(routes)
   ],
 })
